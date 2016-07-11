2b03893e8a4bcd73499a7717eca15fdbb1a5ee3e
igorrozani <igorrozani@gmail.com>
2016-07-10 21:48:32 -0300

    Installing Express and changing event model

--- backend/model/event.js
+++ backend/model/event.js
@@ -1,11 +1,34 @@
-var eventSchema= new mongoose.Schema({
-  name: {type: String, trim: true, required: true},
-  description: {type:String, trim:true},
-  date: {type: Date, required:true},
-  isOnline: {type: Boolean, required: true },
-  type: {type: String, trim:true, required: true},
-  subject:{type:String, trim:true, required:true},
-  keyword: {type:[String]}
+var eventSchema = new Mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    type: {
+        name: type: String,
+        trim: true,
+        required: true,
+        isOnline: {
+            type: Boolean,
+            required: true
+        }
+    },
+    subject: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    keyword: {
+        type: [String]
+    }
 });
 
-var event = mongoose.model('Events', eventSchema);
+var Event = Mongoose.model('events', eventSchema);
